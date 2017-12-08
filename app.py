e9954609bcccaaf4135b37b8967c7fd7af71e342
PhuNong <32190177+phunong@users.noreply.github.com>
2017-12-08 17:31:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ def makeWebhookResult(req):
         link = row[1]
         time = row[2]
         quality = row[3]
-        speech="Thông Tin Phim:"+"\nTên Phim:\t" +name +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality
+#         speech="Thông Tin Phim:"+"\nTên Phim:\t" +name +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality
     con.close()
-    # speech="Thông Tin Phim:"+"\nTên Phim:\t" +name +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality
+    speech="Thông Tin Phim:"+"\nTên Phim:\t" +name +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality
     # print(speech)
     print("Response:")
     print(speech)
