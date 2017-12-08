950c5dc8133884fb861d480d379e946223f98312
PhuNong <32190177+phunong@users.noreply.github.com>
2017-12-08 17:09:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,71 @@
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+# import urllib
+# import json
+# import os
+# import sqlite3
+# import csv
+# import sqlite3 as lite
+
+# from flask import Flask
+# from flask import request
+# from flask import make_response
+
+# # Flask app should start in global layout
+# app = Flask(__name__)
+# @app.route('/welcome')
+# def welcome():
+#     return "Welcome to service!"
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     req = request.get_json(silent=True, force=True)
+
+#     print("Request:")
+#     print(json.dumps(req, indent=4))
+    
+#     res = makeWebhookResult(req)
+
+#     res = json.dumps(res, indent=4)
+#     print(res)
+#     r = make_response(res)
+#     r.headers['Content-Type'] = 'application/json'
+#     return r
+
+# def makeWebhookResult(req):
+#     if req.get("result").get("action") != "filminfo":
+#         return {}
+#     result = req.get("result")
+#     parameters = result.get("parameters")
+#     filmname = parameters.get("film_name")
+# #  load
+#     con = sqlite3.connect('test.db')
+#     cur = con.cursor()
+#     con.text_factory = str
+#     cur.execute("SELECT * FROM film_info WHERE film_name='%s'" %filmname)
+#     rows = cur.fetchall()
+#     for row in rows:
+#         name = row[0]
+#         link = row[1]
+#         time = row[2]
+#         quality = row[3]
+# #         speech ="Thông tin" +filmname
+#     con.close()
+#     speech="Thông Tin Phim:"+"\nTên Phim:\t" +name +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality
+#     print("Response:")
+#     print(speech)
+#     return {
+#         "speech": speech,
+#         "displayText": speech,
+# #         "data": speech,
+# #         "contextOut":speech,
+#         "source": "NVPBOT"
+#     }
+# if __name__ == '__main__':
+#     port = int(os.getenv('PORT', 5000))
+
+#     print ("Starting app on port %d" %(port))
+
+#     app.run(debug=True, port=port, host='0.0.0.0')
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import urllib
@@ -38,23 +106,18 @@ def makeWebhookResult(req):
     result = req.get("result")
     parameters = result.get("parameters")
     filmname = parameters.get("film_name")
-    ss = filmname.get("$film_name")
 #  load
-    con = sqlite3.connect('test.db')
-        cur = con.cursor()
-        con.text_factory = str
-        film = ('The Girl Shaman',)
-        cur.execute("SELECT * FROM film_info WHERE film_name='%s'" %film)
-        rows = cur.fetchall()
-            for row in rows:
-                name = row[0]
-                link = row[1]
-                time = row[2]
-                quality = row[3]
+    cur.execute("SELECT * FROM film_info WHERE film_name='%s'" %t)
+    rows = cur.fetchall()
+    for row in rows:
+        name = row[0]
+        link = row[1]
+        time = row[2]
+        quality = row[3]
+        speech="Thông Tin Phim:"+"\nTên Phim:\t" +name +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality
     con.close()
-    speech="Thông Tin Phim:"+"\nTên Phim:\t" +filmname +"\nTên Phim 2:\t" +name +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality                
-#     con.close()
-#     speech="Thông Tin Phim:"+"\nTên Phim:\t" +filmname +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality
+    # speech="Thông Tin Phim:"+"\nTên Phim:\t" +name +"\t\nLink:\t"+link +"\t\nThời Lượng:\t"+time +"\t\nChất Lượng:\t"+quality
+    # print(speech)
     print("Response:")
     print(speech)
     return {
